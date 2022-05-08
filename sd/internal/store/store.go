a1731d73c19d55075ceaebf6a7b4430141657372
madeline <zzzz465@naver.com>
2022-05-08 21:19:23 +0900

    fix wrong interface method

--- sd/internal/store/store.go
+++ sd/internal/store/store.go
@@ -6,7 +6,7 @@ import "github.com/zzzz465/portal/sd/internal/types"
 // example: InMemoryStore, RedisStore, FileStore, etc...
 // TODO: add WriteMany
 type Store interface {
-	GetRecord(key string) ([]types.Record, error)
+	GetRecord(key string) (*types.Record, error)
 	WriteRecord(key string, record types.Record) error
 	DeleteRecord(key string) error
 }
