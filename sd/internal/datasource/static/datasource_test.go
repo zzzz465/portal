5c4b9ca68c1f6ec4661d5b09c72c61c846f51416
madeline <zzzz465@naver.com>
2022-05-10 17:06:01 +0900

    add static datasource test

--- sd/internal/datasource/static/datasource_test.go
+++ sd/internal/datasource/static/datasource_test.go
@@ -2,7 +2,9 @@ package static_test
 
 import (
     "github.com/spf13/viper"
+    "github.com/stretchr/testify/assert"
     "github.com/zzzz465/portal/sd/internal/datasource/static"
+    "log"
     "strings"
     "testing"
 )
@@ -18,8 +20,18 @@ datasource:
         values:
             - name: site-a.example.com
               tags:
-                region: seoul
-                
+                region: Seoul
+            - name: site-b.example.com
+              tags:
+                region: California
+            - name: portal.domain.com
+              tags:
+                region: Tokyo
+                service: A
+            - name: surf.domain.com
+              tags:
+                region:  New York
+                service: B
 `
 
     err := staticDataSource.ReadConfig(strings.NewReader(cfg))
@@ -27,10 +39,38 @@ datasource:
         t.Error(err)
     }
 
+    log.Println(staticDataSource.Get("datasource.static.values"))
+
     ds := static.NewDataSource(staticDataSource)
 
     records, err := ds.FetchRecords()
     if err != nil {
         t.Error(err)
     }
+
+    // index 0 test
+    assert.Equal(t, records[0].Name, "site-a.example.com")
+    assert.NotNilf(t, records[0].Metadata, "metadata should not be nil.")
+    assert.Equal(t, records[0].Metadata.DataSource, "static")
+    assert.Equal(t, records[0].Metadata.Tags["region"], "Seoul")
+
+    // index 0 test
+    assert.Equal(t, records[0].Name, "site-b.example.com")
+    assert.NotNilf(t, records[0].Metadata, "metadata should not be nil.")
+    assert.Equal(t, records[0].Metadata.DataSource, "static")
+    assert.Equal(t, records[0].Metadata.Tags["region"], "California")
+
+    // index 0 test
+    assert.Equal(t, records[0].Name, "portal.domain.com")
+    assert.NotNilf(t, records[0].Metadata, "metadata should not be nil.")
+    assert.Equal(t, records[0].Metadata.DataSource, "static")
+    assert.Equal(t, records[0].Metadata.Tags["region"], "Tokyo")
+    assert.Equal(t, records[0].Metadata.Tags["service"], "A")
+
+    // index 0 test
+    assert.Equal(t, records[0].Name, "surf.domain.com")
+    assert.NotNilf(t, records[0].Metadata, "metadata should not be nil.")
+    assert.Equal(t, records[0].Metadata.DataSource, "static")
+    assert.Equal(t, records[0].Metadata.Tags["region"], "New York")
+    assert.Equal(t, records[0].Metadata.Tags["service"], "B")
 }
