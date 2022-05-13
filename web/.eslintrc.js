4273bee0796cff73184122803437a4f8b95a862d
madeline <zzzz465@naver.com>
2022-05-13 16:53:46 +0900

    web: update eslint, fix crlf to lf

---
+++ web/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  // plugins: ["prettier"],
+  extends: ['next/core-web-vitals' /* "prettier" */],
+  rules: {
+    // A temporary hack related to IDE not resolving correct package.json
+    'import/no-extraneous-dependencies': 'off',
+    // Since React 17 and typescript 4.1 you can safely disable the rule
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    // indent option conflicts with prettier
+    indent: ['off', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-case-declarations': 'error',
+    'no-underscore-dangle': 'off',
+    'prettier/prettier': 'warn',
+    'no-restricted-syntax': ['off'],
+    '@typescript-eslint/no-namespace': ['off'],
+    'func-names': ['off'],
+    'react/jsx-props-no-spreading': [
+      'warn',
+      {
+        html: 'enforce',
+        custom: 'ignore',
+      },
+    ],
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    createDefaultProgram: true,
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+  },
+}
