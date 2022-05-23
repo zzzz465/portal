21aafbe1aa4a64bfb28c02b7f4b9ed279fd3099a
zzzz465 <zzzz465@naver.com>
2022-05-24 00:36:12 +0900

    web: use ENV for selecting backend url

---
+++ web/src/pages/api/const.ts
@@ -0,0 +1,1 @@
+export const BACKEND_SERVER_URL = process.env.BACKEND_SERVER_URL
---
+++ web/src/pages/api/index.ts
@@ -0,0 +1,4 @@
+import axios from 'axios'
+import { BACKEND_SERVER_URL } from './const'
+
+axios.defaults.baseURL = BACKEND_SERVER_URL
