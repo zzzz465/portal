26616e7450f96015a3971717167a6fe5c49b1810
zzzz465 <zzzz465@naver.com>
2022-05-24 01:36:03 +0900

    web: add /api//records

--- web/src/pages/api/records/index.ts
+++ web/src/pages/api/records/index.ts
@@ -1,12 +1,20 @@
+import axios, { AxiosError } from 'axios'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Record } from '../../../types/record'
+import { tryP } from '../../../utils/try'
+import { BACKEND_SERVER_URL } from '../const'
 
-export type RecordsResponse = {
-  records: Record[]
-}
+export type RecordsResponse = Record[]
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<RecordsResponse>) {
-  return res.status(200).json({
-    records: [],
-  })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<RecordsResponse | any>) {
+  const [recordsResponse, err0] = await tryP(() =>
+    axios.get<RecordsResponse>('/records/', { baseURL: BACKEND_SERVER_URL })
+  )
+  if (err0 != null) {
+    return res.status(500).send((err0 as AxiosError).cause)
+  }
+
+  const records = recordsResponse.data
+
+  return res.status(200).json({ records })
 }
