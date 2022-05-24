fefaeae8935f81766f95433a7a154779c4d19028
madeline <zzzz465@naver.com>
2022-05-24 20:33:55 +0900

    web: add recordItem type

---
+++ web/src/types/recordItem.ts
@@ -0,0 +1,28 @@
+import { Record } from './record'
+
+export type RecordItemType = RecordItem | GroupedRecordItem | WellKnownRecordItem
+export type DisplayableRecordItemType = WellKnownRecordItem | GroupedRecordItem
+
+/**
+ * RecordItem is an element that is grouped by tags.
+ */
+export interface RecordItem {
+  type: 'RecordItem'
+  data: Record
+}
+
+/**
+ * WellKnownRecordItem is an element that can be displayed alone.
+ */
+export type WellKnownRecordItem = RecordItem & {
+  type: 'WellKnownRecordItem'
+}
+
+/**
+ * GroupedRecordItem is an element that holds multiple RecordItem
+ */
+export interface GroupedRecordItem {
+  type: 'groupedRecordItem'
+  tag: string
+  items: RecordItem[]
+}
