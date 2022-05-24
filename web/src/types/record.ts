b63bb0bf2c34785b55d102d6e358f497716e3fcf
madeline <zzzz465@naver.com>
2022-05-24 20:34:01 +0900

    web: add knownLabels

--- web/src/types/record.ts
+++ web/src/types/record.ts
@@ -1,3 +1,5 @@
+import { RecordItemType } from './recordItem'
+
 type _Record = {
   name: string
   tags: string[]
@@ -8,6 +10,10 @@ export type Metadata = {
   dataSource: string
   labels: Record<string, string>
 }
+
+export type KnownLabels = Partial<{
+  recordType: RecordItemType['type']
+}>
 
 // conflict with es5.Record
 export type { _Record as Record }
